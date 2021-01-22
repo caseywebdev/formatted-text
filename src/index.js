e84aeaa8dc68094f6c065fc08d54d5b5d18826e8
Casey Foster <c@sey.me>
2021-01-22 17:04:28 -0600

    Handle headers, 0.6.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { Fragment, createElement } from 'react';
 
-const LINK = /(?:\S+)(\w+:\/\/)\S*|[^\s.]+(\.[a-z-]{2,63})+\S*/gi;
+const linkRegexp = /(?:\S+)(\w+:\/\/)\S*|[^\s.]+(\.[a-z-]{2,63})+\S*/gim;
 
-const DEFAULT_PROTOCOL = 'http://';
+const headerRegexp = /^(#+)[^\S\r\n]*[^#\s].*$/gm;
 
-const TERMINATORS = '.,;:?!';
+const defaultProtocol = 'http://';
 
-const WRAPPERS = {
+const terminators = '.,;:?!';
+
+const wrappers = {
   '(': ')',
   '[': ']',
   '"': '"',
@@ -16,15 +18,29 @@ const WRAPPERS = {
 
 const unwrap = (text, index) => {
   const [first, last] = [text[0], text[text.length - 1]];
-  if (TERMINATORS.indexOf(last) > -1) return unwrap(text.slice(0, -1), index);
-  if (WRAPPERS[first] === last) return unwrap(text.slice(1, -1), index + 1);
+  if (terminators.indexOf(last) > -1) return unwrap(text.slice(0, -1), index);
+  if (wrappers[first] === last) return unwrap(text.slice(1, -1), index + 1);
   return [text, index];
 };
 
-const getLinks = text => {
-  const links = [];
+const comparator = (a, b) =>
+  a.from < b.from ? -1 : a.from > b.from ? 1 : a.to >= b.to ? -1 : 1;
+
+const getBlocks = text => {
+  const blocks = [];
+
   let match;
-  while ((match = LINK.exec(text))) {
+
+  while ((match = headerRegexp.exec(text))) {
+    blocks.push({
+      type: 'header',
+      from: match.index,
+      to: match.index + match[0].length,
+      props: { level: match[1].length }
+    });
+  }
+
+  while ((match = linkRegexp.exec(text))) {
     let [all, protocol, tld] = match;
 
     // To qualify as a link, either the protocol or TLD must be specified.
@@ -32,52 +48,65 @@ const getLinks = text => {
 
     let { index } = match;
     [all, index] = unwrap(all, index);
-    links.push({
-      index,
-      children: all,
-      href: protocol
-        ? all
-        : all.includes('@')
-        ? `mailto:${all}`
-        : DEFAULT_PROTOCOL + all
+    blocks.push({
+      type: 'link',
+      from: index,
+      to: index + all.length,
+      props: {
+        href: protocol
+          ? all
+          : all.includes('@')
+          ? `mailto:${all}`
+          : defaultProtocol + all
+      }
     });
   }
-  return links;
+
+  return blocks.sort(comparator);
 };
 
-const renderLinks = ({ LinkComponent, text }) => {
-  const links = getLinks(text);
-  if (!links.length) return text;
+const renderBlocks = ({ blocks, render, text }) => {
+  if (!blocks.length) return [text];
 
-  return links.reduce(
-    (parts, { index, children, href }, i, { length }) => {
-      const from = index;
-      const to = from + children.length;
-      return {
-        index: to,
-        components: parts.components.concat(
-          text.slice(parts.index, from),
-          createElement(LinkComponent, { href, key: i }, children),
-          i === length - 1 ? text.slice(to) : []
-        )
-      };
-    },
-    { index: 0, components: [] }
-  ).components;
-};
+  const components = [text.slice(0, blocks[0].from)];
+  for (let i = 0; i < blocks.length; ++i) {
+    const { from, to, type, props } = blocks[i];
+    const subtext = text.slice(from, to);
+    const subBlocks = [];
+    for (let j = i + 1; j < blocks.length; ++j) {
+      const subBlock = blocks[j];
+      if (subBlock.to > to) break;
 
-export default ({ children, LinkComponent = 'a' }) => {
-  if (typeof children !== 'string') return null;
-
-  return children
-    .trim()
-    .split('\n')
-    .map((line, i, lines) =>
-      createElement(
-        Fragment,
-        { key: i },
-        renderLinks({ LinkComponent, text: line }),
-        i < lines.length - 1 ? createElement('br') : null
-      )
+      subBlocks.push({
+        ...subBlock,
+        from: subBlock.from - from,
+        to: subBlock.to - from
+      });
+      ++i;
+    }
+    const children = renderBlocks({ blocks: subBlocks, render, text: subtext });
+    components.push(
+      createElement(Fragment, { key: i }, render({ children, type, ...props }))
     );
+
+    components.push(text.slice(to, blocks[i + 1]?.from));
+  }
+
+  return components;
 };
+
+const defaultRender = ({ type, children, ...props }) => {
+  if (type === 'link') return createElement('a', props, children);
+
+  if (type === 'header') {
+    children = [children[0].replace(/^#*\S/, ''), ...children.slice(1)];
+    return createElement(`h${Math.min(props.level, 6)}`, { children });
+  }
+
+  return null;
+};
+
+export default ({ children: text, render = defaultRender }) =>
+  typeof text === 'string'
+    ? renderBlocks({ blocks: getBlocks(text), render, text })
+    : null;
