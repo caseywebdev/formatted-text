c58328f257dc36ff1e31b37885d543d5a5a39121
Casey Foster <c@sey.me>
2024-03-16 10:09:12 -0500

    Long first, 0.6.8

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import { Fragment, createElement } from 'react';
 
 const local = "[\\w!#$%&'*+/=?^{|}~-]+";
-const tlds = [
-  'au',
-  'biz',
-  'ca',
-  'co',
-  'com',
-  'me',
-  'net',
-  'org',
-  'org',
-  'uk',
-  'us'
-];
+const tlds = ['biz', 'com', 'net', 'org', 'au', 'ca', 'co', 'me', 'uk', 'us'];
 const domain = '(?:[a-z]([a-z0-9-]*[a-z0-9])?)';
 const linkRegexp = new RegExp(
   // local email part or scheme
